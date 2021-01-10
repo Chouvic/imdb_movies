7d3dc8d7afc292fecad3a56d59d4ba8239045dda
Jun <jun.zhou.ml@gmail.com>
2021-01-10 13:08:55 +0000

    Add search filter

--- movie_link/views.py
+++ movie_link/views.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+from rest_framework import filters as drf_filters
 from rest_framework import viewsets
 
 from .models import MovieInfo
@@ -9,21 +10,16 @@ from .serializers import MovieInfoSerializer
 
 
 class MovieInfoFilter(filters.FilterSet):
-    _order = filters.OrderingFilter(
-        fields='__all__'
-    )
-
     class Meta:
         model = MovieInfo
         fields = '__all__'
 
 
 class MovieInfoViewSet(viewsets.ModelViewSet):
-    permission_classes = (
-    )
-    authentication_classes = (
-    )
+    permission_classes = ()
+    authentication_classes = ()
     filterset_class = MovieInfoFilter
-
     queryset = MovieInfo.objects.all()
     serializer_class = MovieInfoSerializer
+    filter_backends = (drf_filters.SearchFilter, filters.DjangoFilterBackend)
+    search_fields = ['production_companies', 'title', 'wiki_abstract']
